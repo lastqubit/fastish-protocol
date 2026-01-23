fc8e27d32da6d527cce7d26b5dbea4fa64a393c9
Johan <lastqubit@gmail.com>
2026-01-23 22:28:34 +0100

    command utils

---
+++ contracts/Lib/Utils/Command.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.33;
+
+import {ENTRY, NEXT} from "../Commands/Base.sol";
+import {SELECTOR as ACT} from "../Commands/Core/Act.sol";
+import {SELECTOR as ADD} from "../Commands/Core/Add.sol";
+import {SELECTOR as ALLOW} from "../Commands/Core/Allow.sol";
+import {SELECTOR as AUTHORIZE} from "../Commands/Core/Authorize.sol";
+import {SELECTOR as CREATE} from "../Commands/Core/Create.sol";
+import {SELECTOR as DENY} from "../Commands/Core/Deny.sol";
+import {SELECTOR as INITIATE} from "../Commands/Core/Initiate.sol";
+import {SELECTOR as RELOCATE} from "../Commands/Core/Relocate.sol";
+import {SELECTOR as REMOVE} from "../Commands/Core/Remove.sol";
+import {SELECTOR as SET} from "../Commands/Core/Set.sol";
+import {SELECTOR as TRANSFER} from "../Commands/Core/Transfer.sol";
+import {SELECTOR as UNAUTHORIZE} from "../Commands/Core/Unauthorize.sol";
+import {SELECTOR as UPDATE} from "../Commands/Core/Update.sol";
+import {SELECTOR as RESOLVE} from "../Commands/Core/Resolve.sol";
+import {SELECTOR as TRANSFORM} from "../Commands/Core/Transform.sol";
+import {SELECTOR as UTILIZE} from "../Commands/Core/Utilize.sol";
+
+function isEntry(bytes4 s) pure returns (bool) {
+    return s == ACT || s == ADD || s == ALLOW || s == AUTHORIZE
+        || s == CREATE || s == DENY || s == INITIATE || s == RELOCATE
+        || s == REMOVE || s == SET || s == TRANSFER || s == UNAUTHORIZE
+        || s == UPDATE;
+}
+
+function isNext(bytes4 s) pure returns (bool) {
+    return s == RESOLVE || s == TRANSFORM || s == UTILIZE;
+}
+
+function isMatch(bytes4 head, bytes4 step) pure returns (bool) {
+    if (head == NEXT) return isNext(step);
+    if (head == ENTRY) return isEntry(step);
+    return head == step;
+}
