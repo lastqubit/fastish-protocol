85b934f79a25bb145afc97237da9e602327258b3
Crypto Qubit <0xcryptoqubit@gmail.com>
2026-01-29 16:49:04 +0100

    added deadline to rush

--- contracts/Lib/Commands/Core/Entry/Pipe.sol
+++ contracts/Lib/Commands/Core/Entry/Pipe.sol
@@ -3,19 +3,23 @@ pragma solidity ^0.8.33;
 
 import {Command} from "../Base.sol";
 
-string constant ABI = "function pipe(bytes[] steps, bytes signed) external payable returns(uint count)";
+string constant ABI = "function pipe(uint192 deadline, bytes[] steps, bytes signed) external payable returns(uint count)";
 bytes4 constant SELECTOR = IPipe.pipe.selector;
 
 interface IPipe {
-    function pipe(bytes[] calldata steps, bytes calldata signed) external payable returns (uint);
+    function pipe(uint192 deadline, bytes[] calldata steps, bytes calldata signed) external payable returns (uint);
 }
 
 abstract contract Pipe is IPipe, Command {
-    uint internal immutable executeId = toEid(SELECTOR);
+    uint internal immutable pipeId = toEid(SELECTOR);
 
     constructor() {
-        emit Endpoint(hostId, executeId, 0, ABI, "");
+        emit Endpoint(hostId, pipeId, 0, ABI, "");
     }
 
-    function pipe(bytes[] calldata steps, bytes calldata signed) external payable virtual returns (uint);
+    function pipe(
+        uint192 deadline,
+        bytes[] calldata steps,
+        bytes calldata signed
+    ) external payable virtual returns (uint);
 }
