b9a6ea175f47e903040d6a19c740f91fbc0d760d
Johan <lastqubit@gmail.com>
2026-03-16 16:09:43 +0100

    Set up test and formatting tooling

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "hardhat/config";
+import hardhatMocha from "@nomicfoundation/hardhat-mocha";
 
 export default defineConfig({
+  plugins: [hardhatMocha],
   solidity: {
     version: "0.8.33",
+  },
+  test: {
+    mocha: {
+      require: ["./test/helpers/matchers.ts"],
+    },
   },
 });
